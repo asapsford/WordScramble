8e99ba6f5669a005333714b59b67b7f9b7092713
Alex Sapsford <asapsford11@gmail.com>
2020-09-24 11:05:13 +0100

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Alex Sapsford on 24/09/2020.
+//  Copyright © 2020 Alex Sapsford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
